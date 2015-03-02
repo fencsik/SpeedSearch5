fec1c439727f8032c6980135e8a55ec634984ba4
David Fencsik <david.fencsik@gmail.com>
2015-03-01 21:58:53 -0800

    Add more processing of keys and values.
    Move processing of keys into a separate function.  Add a function to
    process values, breaking up strings into arrays, converting text into
    numbers, and cell arrays into vectors as appropriate.

--- Test/LoadSettings.m
+++ Test/LoadSettings.m
@@ -73,11 +73,25 @@ function [key, val] = GetNextTokenFromSettingsFile (fid)
         str = strtrim(RemoveComments(str));
         if (~isempty(str))
             tokens = strtrim(strsplit(str, "="));
-            key = tokens{1};
-            val = tokens{2};
+            if (numel(tokens) >= 1)
+                key = ProcessKey(tokens{1});
+            endif
+            if (numel(tokens) >= 2)
+                val = ProcessValue(tokens{2});
+            endif
             break;
         endif
     endwhile
+endfunction
+
+function key = ProcessKey(keyToken)
+    key = StripWhitespaceFromString(keyToken);
+endfunction
+
+function value = ProcessValue(valueToken)
+    value = SplitStringIntoVector(valueToken);
+    value = ConvertToNumbers(value);
+    value = ConvertToVector(value);
 endfunction
 
 function stringOut = RemoveComments(stringIn)
@@ -88,6 +102,72 @@ endfunction
 function stringOut = StripWhitespaceFromString (stringIn)
     # remove all whitespace from a string
     stringOut = regexprep(stringIn, "\\s", "");
+endfunction
+
+function vector = SplitStringIntoVector(stringIn)
+### if stringIn has separate components, split them
+    ## replace array indicators/separators with whitespace
+    stringIn = regexprep(stringIn, "\\[|\\]|,", " ");
+    stringIn = strtrim(stringIn);
+    vector = strsplit(stringIn);
+    if (numel(vector) == 1)
+        vector = vector{1};
+    endif
+endfunction
+
+function vectorOut = ConvertToNumbers(vectorIn)
+### Converts number strings to floats.  Leaves character strings alone
+    if (isempty(vectorIn))
+        vectorOut = vectorIn;
+    elseif (!iscell(vectorIn))
+        n = str2double(vectorIn);
+        if (isnan(n))
+            vectorOut = vectorIn;
+        else
+            vectorOut = n;
+        endif
+    else
+        for (i = 1:numel(vectorIn))
+            n = str2double(vectorIn{i});
+            if (isnan(n)) # conversion to number failed
+                vectorOut{i} = vectorIn{i};
+            else
+                vectorOut{i} = n;
+            endif
+        endfor
+    endif
+endfunction
+
+function vectorOut = ConvertToVector(vectorIn)
+### Converts numeric cell arrays to vectors.  Leaves cell arrays with
+### strings alone.
+    if (iscell(vectorIn) && IsCellArrayNumeric(vectorIn))
+        vectorOut = ConvertNumericCellArrayToVector(vectorIn);
+    else
+        vectorOut = vectorIn;
+    endif
+endfunction
+
+function val = IsCellArrayNumeric(vector)
+    if (iscell(vector))
+        val = 1;
+        for (i = 1:numel(vector))
+            if (ischar(vector{i}))
+                val = 0;
+                break;
+            endif
+        endfor
+    else
+        val = 0;
+    endif
+endfunction
+
+function out = ConvertNumericCellArrayToVector(in)
+    n = numel(in)
+    out = zeros(1, n)
+    for (i = 1:n)
+        out(i) = in{i};
+    endfor
 endfunction
 
 function MakeSettingsGlobal (settingsArray)
